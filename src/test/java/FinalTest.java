37063bd8e8eebbe181d570cd40a19937772f2e70
CalvinChin <45876209+CalvinChin@users.noreply.github.com>
2018-12-14 22:40:35 +0800

    Update FinalTest.java

--- src/test/java/FinalTest.java
+++ src/test/java/FinalTest.java
@@ -22,6 +22,14 @@ public class FinalTest {
 		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("5");
 		assertEquals(9, Memory_Management.FIFO(bay, frame));
 	}
+	
+	@Test
+	public void LRUShouldReturnExpectedNumber() {
+		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("1"); bay.add("2"); bay.add("5"); 
+		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("5");
+		assertEquals(10, Memory_Management.LRU(bay, frame));
+	}
+	
 	@Test
 	public void OptimalShouldReturnExpectedNumber() {
 		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("1"); bay.add("2"); bay.add("5"); 
